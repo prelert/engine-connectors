da36a9fd1e46a44e86236f592381703ae85204b0
sophiec20 <schang@prelert.com>
2014-05-15 18:52:30 +0100

    Upload postgresql connector

---
+++ simple scripts/postgresql-connector.sh
@@ -0,0 +1,67 @@
+#!/bin/sh
+
+# A simple Anomaly Detective Engine connector to analyze a batch of data from a PostgreSQL data store needs to:
+# 1. Create an Engine API job
+# 2. Query the data store
+# 3. Stream data from the data store to the Engine API job
+
+# For example, if time series data is stored in PostgreSQL as follows:
+# dbname=# select time,value from time_series_points where time_series_id=1395 order by time;
+# time                   |   value
+# ================================
+# 2011-03-01 05:01:00+00 |    825
+# 2011-03-01 05:02:00+00 |    513
+# 2011-03-01 05:03:00+00 |    480
+# 2011-03-01 05:04:00+00 |    492
+# 2011-03-01 05:05:00+00 |    473
+
+# Note: IMPORTANT to order by time
+
+PRELERT_API_HOST=localhost
+
+# Create job and record JobId
+PRELERT_JOB_ID=`\
+curl -X POST -H 'Content-Type: application/json' "http://$PRELERT_API_HOST:8080/engine/v0.3/jobs" -d '{
+        "analysisConfig" : {
+        "bucketSpan":3600,
+        "detectors" :[{"function":"max","fieldName":"value"}]
+},
+    "dataDescription" : {
+        "fieldDelimiter":",",
+        "timeField":"time",
+        "timeFormat":"yyyy-MM-dd HH:mm:ssX"
+    }
+}' | awk -F'"' '{ print $4; }' \
+`
+
+echo "Created analysis job $PRELERT_JOB_ID"
+
+echo "Querying PostgreSQL and streaming results to Engine API"
+
+# Query database and stream to Engine API
+psql -F, -A -c "select time,value from time_series_points where time_series_id=1395 order by time;" dbname | \
+curl -X POST -T - "http://$PRELERT_API_HOST:8080/engine/v0.3/data/$PRELERT_JOB_ID"
+
+echo "Done."
+
+# Close job - this will flush analytics results
+curl -X POST "http://$PRELERT_API_HOST:8080/engine/v0.3/data/$PRELERT_JOB_ID/close"
+
+
+# Anomaly detection analysis results are now available to query
+# Note: If streaming data then use asynchronous result processor script
+
+# Get results and print to stdout as csv
+curl "http://$PRELERT_API_HOST:8080/engine/v0.3/results/$PRELERT_JOB_ID?take=1000000" | python -c "
+import json,sys
+
+obj=json.load(sys.stdin)
+
+buckets=obj['documents']
+
+print 'date,id,anomalyScore'
+
+for bucket in buckets:
+    print '{0},{1},{2}'.format(bucket['timestamp'], bucket['id'], bucket['anomalyScore'])
+"
+
